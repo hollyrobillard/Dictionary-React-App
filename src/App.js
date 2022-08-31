e244bb512d43cdd0025ab410bbe11ef6c22715c2
Holly Ann Robillard <103912261+hollyrobillard@users.noreply.github.com>
2022-08-31 09:03:47 -0400

    Noreferrer added to links

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
       </header>
       <footer className="App-footer">
         <div>
-          <a href="https://resonant-kleicha-ad387d.netlify.app" target="_blank">Site</a> Created and Maintained by <a href="https://www.linkedin.com/in/hollyrobillard/" target="_blank">Holly Robillard</a>
+          <a href="https://resonant-kleicha-ad387d.netlify.app" target="_blank" rel="noreferrer">Site</a> Created and Maintained by <a href="https://www.linkedin.com/in/hollyrobillard/" target="_blank" rel="noreferrer">Holly Robillard</a>
         </div>
         <div>
-          Open Source Code on <a href="https://github.com/hollyrobillard/Dictionary-React-App"  target="_blank">Github</a>
+          Open Source Code on <a href="https://github.com/hollyrobillard/Dictionary-React-App"  target="_blank" rel="noreferrer">Github</a>
         </div>
       </footer>
     </div>
