169d62e99398cf76fd55ba311064f82c7baf18b6
Holly Ann Robillard <103912261+hollyrobillard@users.noreply.github.com>
2022-09-01 08:47:26 -0400

    Form set-up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import Logo from "./Logo";
+import Dictionary from "./Dictionary";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <Logo />
-      </header>
-      <footer className="App-footer">
-        <div>
-          <a href="https://resonant-kleicha-ad387d.netlify.app" target="_blank" rel="noreferrer">Site</a> Created and Maintained by <a href="https://www.linkedin.com/in/hollyrobillard/" target="_blank" rel="noreferrer">Holly Robillard</a>
-        </div>
-        <div>
-          Open Source Code on <a href="https://github.com/hollyrobillard/Dictionary-React-App"  target="_blank" rel="noreferrer">Github</a>
-        </div>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+            <Logo />
+        </header>
+        <main>
+          <Dictionary defaultWord="word" />
+        </main>
+        <footer className="App-footer">
+          <div>
+            <a href="https://resonant-kleicha-ad387d.netlify.app" target="_blank" rel="noreferrer">Site</a> Created and Maintained by <a href="https://www.linkedin.com/in/hollyrobillard/" target="_blank" rel="noreferrer">Holly Robillard</a>
+          </div>
+          <div>
+            Open Source Code on <a href="https://github.com/hollyrobillard/Dictionary-React-App"  target="_blank" rel="noreferrer">Github</a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary(props) {
+    const [word, setWord] = useState(props.defaultWord);
+    
+    function search(event) {
+        event.preventDefault();
+    }
+
+    function updateWordChange(event) {
+        setWord(event.target.value);
+    }
+    
+    return (
+        <div className="Dictionary">
+            <h1>Looking for that Word?</h1>
+            <h2>Dictionary Search</h2>
+            <form onSubmit={search}>
+                <input type="search" className="search" onChange={updateWordChange}/>
+                <input type="submit" className="submit" />
+            </form>
+        </div>
+    );
+}
